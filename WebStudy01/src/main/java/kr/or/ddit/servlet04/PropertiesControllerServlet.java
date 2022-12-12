abc28cfa61a69286358b111d7b9c3a7e630e715c
Insoo Park <insoo_p@naver.com>
2022-12-12 14:05:02 +0900

    20221210

--- WebStudy01/src/main/java/kr/or/ddit/servlet04/PropertiesControllerServlet.java
+++ WebStudy01/src/main/java/kr/or/ddit/servlet04/PropertiesControllerServlet.java
@@ -1,7 +1,10 @@
 package kr.or.ddit.servlet04;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,26 +12,42 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import kr.or.ddit.servlet01.DescriptionServlet;
+import kr.or.ddit.servlet04.service.PropertiesService;
+import kr.or.ddit.servlet04.service.PropertiesServiceImpl;
+
 @WebServlet("/03/props/propsView.do")
 public class PropertiesControllerServlet extends HttpServlet{
+	private PropertiesService service = new PropertiesServiceImpl();
+	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		String accept = req.getHeader("Accept"); // */*
 		
-		String accept = req.getHeader("Accept");
-		if(accept.toLowerCase().contains("json")) {
-			// native(DataStore.properties)->json : marshalling
-			// {"prop1":"value1", ....}
-			StringBuffer json = null;
-			resp.setContentType("applicaiotn/json;charset=UTF-8");
-			try(
-			PrintWriter out = resp.getWriter();
-			){
-				out.print(json);				
-			}
-		}else {
-			String path = "/WEB-INF/views/03/propsView.jsp";
-			req.getRequestDispatcher(path).forward(req,resp);			
+		Object target = service.retrieveData();
+		req.setAttribute("target", target);
+		
+		String path = null;
+		
+		if(accept.startsWith("*/*") || accept.toLowerCase().contains("html")) {
+			path = "/WEB-INF/views/03/propsView.jsp";
+		}else if(accept.toLowerCase().contains("json")) {
+			path = "/jsonView.do";
+		}else if(accept.toLowerCase().contains("xml")) {
+			path = "/xmlView.do";
 		}
+		req.getRequestDispatcher(path).forward(req, resp);
 		
 	}
 }
+
+
+
+
+
+
+
--- WebStudy01/src/main/webapp/WEB-INF/views/03/propsView.jsp
+++ WebStudy01/src/main/webapp/WEB-INF/views/03/propsView.jsp
@@ -10,6 +10,14 @@
 <body>
 <h4> properties 파일 뷰어 </h4>
 <!-- <img src="../../resources/images/cat1.jpg" /> -->
+<label>
+	<input type="radio" name="dataType" value="json" checked />JSON
+</label>
+<label>
+	<input type="radio" name="dataType" value="xml" />XML
+</label>
+<button type="button" class="loadData">LOAD DATA</button>
+<button type="button" class="clearData">CLEAR DATA</button>
 <table>
 	<thead>
 		<tr>
@@ -17,22 +25,74 @@
 			<th>value</th>
 		</tr>
 	</thead>
-	<tbody>
+	<tbody id="listBody">
 		
 	</tbody>
 </table>
 <script type="text/javascript">
-	$.ajax({
-		dataType : "json",
-		success : function(resp) {
-			resp['prop1']
+	let listBody = $("#listBody");
+	let dataTypes = $("[name=dataType]");
+	let makeTrTag=function(name, value){
+		let tr = $("<tr>").append(
+				$("<td>").html(name)		
+				, $("<td>").html(value)		
+			);
+		return tr;
+	}
+	let sucesses = {
+		json:function(resp){
+			let trTags = [];
+			$.each(resp, function(name, value){
+				trTags.push(makeTrTag(name, value));
+			});
+			listBody.empty();
+			listBody.append(trTags);
 		},
-		error : function(jqXHR, status, error) {
-			console.log(jqXHR);
-			console.log(status);
-			console.log(error);
+		xml:function(domResp){
+			let root = $(domResp).find("Properties");
+			let trTags = [];
+			root.children().each(function(idx, child){
+				let name = child.tagName;
+				let value = child.innerHTML;
+				let tr = makeTrTag(name, value);
+				trTags.push(tr);
+			});
+			listBody.empty();
+			listBody.append(trTags);
 		}
+	}
+	let btn = $(".loadData").on("click", function(){
+		let dataType = dataTypes.filter(":checked").val();
+		$.ajax({
+			dataType : dataType,
+			success : sucesses[dataType],
+			error : function(jqXHR, status, error) {
+				console.log(jqXHR);
+				console.log(status);
+				console.log(error);
+			}
+		});
+	});
+	let clearBtn = $(".clearData").on("click", function(){
+		listBody.empty();
 	});
 </script>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
